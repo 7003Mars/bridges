8cd1923037febf23223747c63ff3746c80f74bbe
Mars <>
2022-11-17 22:44:49 +0800

    Add drag support for more blocks

--- src/me/mars/ConfigHandler.java
+++ src/me/mars/ConfigHandler.java
@@ -2,12 +2,11 @@ package me.mars;
 
 import arc.Core;
 import arc.Events;
-import arc.func.Func2;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Lines;
 import arc.input.KeyCode;
 import arc.math.geom.Vec2;
-import arc.struct.ObjectMap;
+import arc.struct.ObjectSet;
 import arc.util.Reflect;
 import mindustry.Vars;
 import mindustry.game.EventType.Trigger;
@@ -17,14 +16,15 @@ import mindustry.graphics.Pal;
 import mindustry.input.InputHandler;
 import mindustry.ui.fragments.BlockConfigFragment;
 import mindustry.world.blocks.distribution.BufferedItemBridge.BufferedItemBridgeBuild;
-import mindustry.world.blocks.distribution.ItemBridge;
-import mindustry.world.blocks.distribution.MassDriver;
+import mindustry.world.blocks.distribution.ItemBridge.ItemBridgeBuild;
 import mindustry.world.blocks.distribution.MassDriver.MassDriverBuild;
-
-import java.lang.reflect.Field;
+import mindustry.world.blocks.liquid.LiquidBridge.LiquidBridgeBuild;
+import mindustry.world.blocks.payloads.PayloadMassDriver.PayloadDriverBuild;
 
 public class ConfigHandler {
-	static ObjectMap<Class<? extends Building>, Func2<Building, Building, Boolean>> checkers = new ObjectMap<>();
+//	static ObjectMap<Class<? extends Building>, Func2<Building, Building, Boolean>> checkers = new ObjectMap<>();
+	static ObjectSet<Class<? extends Building>> validBuilds = ObjectSet.with(
+			BufferedItemBridgeBuild.class, ItemBridgeBuild.class, LiquidBridgeBuild.class, MassDriverBuild.class, PayloadDriverBuild.class);
 
 	static Building configClicked;
 	private static BlockConfigFragment configFragment;
@@ -32,26 +32,8 @@ public class ConfigHandler {
 	public static void init() {
 		// Jank time
 		configFragment = new ModifiedConfigFragment();
-//		Field field = Vars.control.input.getClass().getDeclaredField("config");
 
 		Reflect.set(InputHandler.class, Vars.control.input, "config", configFragment);
-
-
-		checkers.put(BufferedItemBridgeBuild.class,
-				(b1, b2) -> ((ItemBridge)b1.block).linkValid(b1.tile(), b2.tile(), false));
-		checkers.put(MassDriverBuild.class,
-				(b1, b2) -> b1.block == b2.block && b1.team == b2.team && ((MassDriver)b1.block).range > b1.dst(b2));
-
-//		Events.run(Trigger.update, () -> {
-//
-//			if (configFragment.isShown() && configClicked == null) {
-//				Building selected = configFragment.getSelected();
-//				if (checkers.containsKey(selected.getClass())) configClicked = selected;
-//			} else if (!configFragment.isShown()){
-//				configClicked = null;
-//			}
-//
-//		});
 
 		Events.run(Trigger.uiDrawBegin, () -> {
 			if (configClicked != null) {
@@ -67,29 +49,17 @@ public class ConfigHandler {
 						configClicked = null;
 					} else if (hovered == null) {
 						configFragment.hideConfig();
-//						configClicked = null;
-					} else if (checkers.get(configClicked.getClass()).get(configClicked, hovered)) {
+					} else {
 						configClicked.onConfigureBuildTapped(hovered);
-//						configClicked = null;
 						configFragment.hideConfig();
 					}
-					// HIde if: Configured or
-					// Dont hide if its itself
-//					if (hovered != configClicked && checkers.get(configClicked.getClass()).get(configClicked, hovered)) {
-//						configClicked.onConfigureBuildTapped(hovered);
-//						configClicked = null;
-//						configFragment.hideConfig();
-//					} else if (hovered != configClicked) {
-//						configClicked = null;
-//						configFragment.hideConfig();
-//					}
 				}
 			}
 		});
 	}
 
 	static void setBuild(Building build) {
-		if (checkers.containsKey(build.getClass())) configClicked = build;
+		if (validBuilds.contains(build.getClass())) configClicked = build;
 	}
 }
 
