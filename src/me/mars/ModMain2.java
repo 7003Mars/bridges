36fb65a850111c7f379bb9b21d0f651286dc7be5
Mars <>
2022-12-14 21:40:09 +0800

    "Some changes" and lots of pain

--- src/me/mars/ModMain2.java
+++ src/me/mars/ModMain2.java
@@ -10,10 +10,12 @@ import arc.math.geom.Rect;
 import arc.math.geom.Vec2;
 import arc.scene.style.TextureRegionDrawable;
 import arc.scene.ui.layout.Table;
+import arc.struct.IntIntMap;
 import arc.struct.Seq;
 import arc.util.*;
 import mindustry.Vars;
 import mindustry.game.EventType.*;
+import mindustry.gen.Groups;
 import mindustry.gen.Icon;
 import mindustry.mod.Mod;
 import mindustry.world.Block;
@@ -21,7 +23,7 @@ import mindustry.world.Tile;
 import mindustry.world.blocks.distribution.ItemBridge;
 import mindustry.world.blocks.distribution.ItemBridge.ItemBridgeBuild;
 
-import static mindustry.Vars.tilesize;
+import static mindustry.Vars.*;
 //import arc.util.noise.Noise;
 
 
@@ -29,22 +31,20 @@ public class ModMain2 extends Mod {
 	public static boolean debugMode;
 	public static int lineOpacity;
 	public static boolean fixedColor;
+	public static int ticksPerUpdate = 10;
 
 	static Rect bounds = new Rect(0, 0, 0, 0);
 	public static QuadTree<Segment> vertSeg = new QuadTree<>(bounds);
 	public static QuadTree<Segment> horiSeg = new QuadTree<>(bounds);
 	public static Seq<Segment> allSegments = new Seq<>(false);
 
-	// TODO: Yeah horrible. Valve pls fix
-	public static Seq<ItemBridgeBuild> bridgeCache = new Seq<>(false);
-
 	public static Seq<ItemBridge> bridgeBlocks = new Seq<>(); // All bridges on the current world
 	public static ItemBridge currentSelection = null;
 	private static Seq<Segment> hoverSelected = new Seq<>(false, 8, Segment.class);
 
-//	private static Seq<Runnable> queue = new Seq<>();
-//	public static IntIntMap lastConfigs = new IntIntMap();
-
+	private static float time;
+	private static Seq<Runnable> queue = new Seq<>();
+	public static IntIntMap lastConfigs = new IntIntMap();
 	@Override
 	public void init() {
 		if (Vars.headless) return;
@@ -111,183 +111,167 @@ public class ModMain2 extends Mod {
 					// For Select, index starts at 1
 					Select.instance().select(hoverSelected.items, Structs.comparingFloat(segment -> segment.start.pos()),
 							index+1, hoverSelected.size).drawHighlight();
-//					hoverSelected.sort(segment -> segment.start.pos());
-//					hoverSelected.get(index).drawHighlight();
 				}
 			}
 		});
-
-		Timer.schedule(() -> {
-			// Settings
-			lineOpacity = Core.settings.getInt("bridging.line-opacity");
-			fixedColor = Core.settings.getBool("bridging.fixed-highlight-color");
-			debugMode = Core.settings.getBool("bridging.debug-mode");
-
-			if (debugMode) Time.mark();
-			update();
-			if (debugMode) Vars.ui.showInfoToast("Took " + Time.elapsed() + " ms to update", 1);
-		}, 0f, 1f);
 	}
 
 	public static void eventInit() {
 		Events.on(WorldLoadEvent.class, worldLoadEvent -> {
-			Vars.world.getQuadBounds(bounds);
+			bounds.set(0, 0, world.width(), world.height());
 			Log.info("Bounds: @", bounds);
 			reloadSegments();
 		});
 
-		Events.on(BlockBuildEndEvent.class, blockBuildEndEvent -> {
-			if (blockBuildEndEvent.tile.build instanceof ItemBridgeBuild bridge) bridgeCache.add(bridge);
-		});
-		Events.on(PayloadDropEvent.class, payloadDropEvent -> {
-			if (payloadDropEvent.build instanceof ItemBridgeBuild bridge) bridgeCache.add(bridge);
+		// These work on the assumption that bridges are 1x1
+		Events.on(TilePreChangeEvent.class, tilePreChangeEvent -> {
+			if (tilePreChangeEvent.tile.build instanceof ItemBridgeBuild bridge) bridgeRemoved(bridge);
 		});
 
-		// TODO: Pain and suffering.
-//		Events.on(BlockBuildEndEvent.class, blockBuildEndEvent -> {
-//			if (!(blockBuildEndEvent.tile.build instanceof ItemBridgeBuild bridge)) return;
-//			lastConfigs.put(bridge.pos(), bridge.link);
-//			// Form for link
-//			Segment segment = findSeg(Point2.x(bridge.link), Point2.y(bridge.link), Segment.linkDir(bridge));
-//			if (segment == null || segment.linkDir() != Segment.linkDir(bridge)) {
-//				formSegment(bridge);
-//			} else {
-//				segment.start = bridge;
-//				segment.updateEnd();
-//			}
-//			queue.add(() -> {
-//				// Form for incoming
-//				for (int i = 0; i < bridge.incoming.size; i++) {
-//					int pos = bridge.incoming.items[i];
-//					Seq<Segment> tempArr = new Seq<>();
-//					both(tree -> tree.intersect(Point2.x(pos), Point2.y(pos), 1, 1, tempArr));
-//					tempArr.isEmpty();
-//					both(tree -> tree.intersect(Point2.x(pos), Point2.y(pos), 1, 1, Segment::updateEnd));
-//					formSegment(pos);
-//				}
-//			});
-//		});
-//
-//		Events.on(TilePreChangeEvent.class, tileChangeEvent -> {
-//			if (!(tileChangeEvent.tile.build instanceof ItemBridgeBuild bridge)) return;
-//			// TODO: Unsure if it should be run the next tick
-//			lastConfigs.remove(bridge.pos());
-//			// Remove itself
-//			Segment self = findSeg(bridge.tileX(), bridge.tileY(), Segment.linkDir(bridge));
-//			if (self != null) {
-//				getTree(self.linkDir()).remove(self);
-//				allSegments.remove(self);
-//			}
-//			// Make new Segment for link if possible
-//			if (Vars.world.build(bridge.link) instanceof ItemBridgeBuild link) {
-//				link.incoming.removeValue(bridge.pos()); // TODO: Shouldn't it already be removed
-//				formSegment(link);
-//			}
-//			// Update those linked to the bridge.
-//			both(tree -> tree.intersect(bridge.tileX(), bridge.tileY(), 1, 1, segment -> {
-//				ItemBridgeBuild oldEnd = segment.end;
-//				// Jank
-//				int removeIndex = segment.passing.indexOf(bridge.pos());
-//				if (removeIndex > 0) {
-//					segment.end = (ItemBridgeBuild) Vars.world.build(segment.passing.items[removeIndex-1]);
-//					segment.passing.setSize(removeIndex);
-//				}
-//				// - - - - -
-//				// 0 1 2 3 4
-//				if (!segment.valid()) {
-//					ItemBridgeBuild before = segment.end;
-//					segment.end = oldEnd; // Needed as QuadTree#remove needs the Segment hitbox
-//					tree.remove(segment);
-//					allSegments.remove(segment);
-//					Seq<Segment> temp = new Seq<>();
-//					both(tree2 -> tree2.getObjects(temp));
-//					if (temp.contains(segment)) {
-//						tree.remove(segment);
-//					};
-//			}}));
-//		});
-//
-//		Events.on(ConfigEvent.class, configEvent -> {
-//			if (!(configEvent.tile instanceof ItemBridge.ItemBridgeBuild bridge)) return;
-//
-//			// Form for disconnected
-//			int lastConfig = lastConfigs.get(bridge.pos(), -1); //
-////			if (lastConfig != -1) formSegment(lastConfig);
-//			if (Vars.world.build(lastConfig) instanceof ItemBridgeBuild oldLink) {
-////				if (oldLink.incoming.contains(bridge.pos())) Log.info("Removing");
-//				oldLink.incoming.removeValue(bridge.pos());
-//				formSegment(oldLink);
-//			}
-//			// Form for new connection
-//			if (Vars.world.build((int) configEvent.value) instanceof ItemBridgeBuild link) {
-//				bridge.updateTile();
-//				link.incoming.add(bridge.pos());
-//				if (!segHead(link)) {
-//					Segment linkSeg = findSeg(link.tileX(), link.tileY(), 0);
-//					if (linkSeg != null) {
-//						allSegments.remove(linkSeg);
-//						getTree(linkSeg.linkDir()).remove(linkSeg);
-//					}
-//				}
-//				// Remove to prevent duplicates
-//				link.incoming.removeValue(bridge.pos());
-//				formSegment((ItemBridgeBuild) configEvent.tile);
-//			}
-//			formSegment(bridge);
-//			// Update those passing
-//			both(tree -> tree.intersect(bridge.tileX(), bridge.tileY(), 1, 1, segment -> {
-//				ItemBridgeBuild oldEnd = segment.end;
-//				segment.updateEnd();
-//				if (!segment.valid()) {
-//					segment.end = oldEnd; // Needed as QuadTree#remove needs the Segment hitbox
-//					tree.remove(segment);
-//					allSegments.remove(segment);
-//				}
-//			}));
-//			lastConfigs.put(bridge.pos(), (int) configEvent.value);
-//		});
-//
-//		// Wait 1 tick for incoming to be updated
-//		Events.run(Trigger.update, () -> {
-//			queue.forEach(Runnable::run);
-//			queue.clear();
-//		});
+		Events.on(TileChangeEvent.class, tileChangeEvent -> {
+			if (tileChangeEvent.tile.build instanceof ItemBridgeBuild bridge) bridgeBuilt(bridge);
+		});
+
+		Events.on(ConfigEvent.class, configEvent -> {
+			if (!(configEvent.tile instanceof ItemBridge.ItemBridgeBuild bridge)) return;
+
+			// Form for disconnected
+			int lastConfig = lastConfigs.get(bridge.pos(), -1); //
+//			if (lastConfig != -1) formSegment(lastConfig);
+			if (Vars.world.build(lastConfig) instanceof ItemBridgeBuild oldLink) {
+//				if (oldLink.incoming.contains(bridge.pos())) Log.info("Removing");
+				oldLink.incoming.removeValue(bridge.pos());
+				formSegment(oldLink);
+			}
+			// Form for new connection
+			if (Vars.world.build((int) configEvent.value) instanceof ItemBridgeBuild link) {
+				bridge.updateTile();
+				link.incoming.add(bridge.pos());
+				if (!segHead(link)) {
+					Segment linkSeg = findSeg(link.tileX(), link.tileY(), 0);
+					if (linkSeg != null) {
+						allSegments.remove(linkSeg);
+						getTree(linkSeg.linkDir()).remove(linkSeg);
+					}
+				}
+				// Remove to prevent duplicates
+				link.incoming.removeValue(bridge.pos());
+				formSegment((ItemBridgeBuild) configEvent.tile);
+			}
+			formSegment(bridge);
+			// Update those passing
+			both(tree -> tree.intersect(bridge.tileX(), bridge.tileY(), 1, 1, segment -> {
+				ItemBridgeBuild oldEnd = segment.end;
+				updateEnd(segment);
+				if (!segment.valid()) {
+					segment.end = oldEnd; // Needed as QuadTree#remove needs the Segment hitbox
+					tree.remove(segment);
+					allSegments.remove(segment);
+				}
+			}));
+			lastConfigs.put(bridge.pos(), (int) configEvent.value);
+		});
+
+		// Wait 1 tick for incoming to be updated
+		Events.run(Trigger.update, () -> {
+			time+=Time.delta;
+			if (time >= ticksPerUpdate) {
+				// Settings
+				lineOpacity = Core.settings.getInt("bridging.line-opacity");
+				fixedColor = Core.settings.getBool("bridging.fixed-highlight-color");
+				debugMode = Core.settings.getBool("bridging.debug-mode");
+
+				if (debugMode) Time.mark();
+				update();
+				if (debugMode) Vars.ui.showInfoToast("Took " + Time.elapsed() + " ms to update", 1);
+				time-=ticksPerUpdate;
+			}
+			queue.each(Runnable::run);
+			queue.clear();
+		});
 	}
 
-	public void update() {
+	public static void bridgeBuilt(ItemBridgeBuild bridge) {
+		queue.add(() -> {
+			lastConfigs.put(bridge.pos(), bridge.link);
+			// Form for link
+			Segment segment = findSegStrict(Point2.x(bridge.link), Point2.y(bridge.link), Segment.linkDir(bridge));
+			if (segment == null) {
+				formSegment(bridge);
+			} else {
+				segment.start = bridge;
+				updateEnd(segment);
+			}
+			// Form for incoming
+			for (int i = 0; i < bridge.incoming.size; i++) {
+				int pos = bridge.incoming.items[i];
+				both(tree -> tree.intersect(Point2.x(pos), Point2.y(pos), 1, 1, ModMain2::updateEnd));
+				formSegment(pos);
+			}
+		});
+	}
+
+	public static void bridgeRemoved(ItemBridgeBuild bridge) {
+		lastConfigs.remove(bridge.pos());
+		// Remove itself
+		Segment self = findSeg(bridge.tileX(), bridge.tileY(), Segment.linkDir(bridge));
+		if (self != null) {
+			getTree(self.linkDir()).remove(self);
+			allSegments.remove(self);
+		}
+		// Make new Segment for link if possible
+		if (Vars.world.build(bridge.link) instanceof ItemBridgeBuild link) {
+			link.incoming.removeValue(bridge.pos());
+			formSegment(link);
+		}
+		// Update those linked to the bridge.
+		both(tree -> tree.intersect(bridge.tileX(), bridge.tileY(), 1, 1, segment -> {
+			ItemBridgeBuild oldEnd = segment.end;
+			// Jank
+			int removeIndex = segment.passing.indexOf(bridge.pos());
+			if (removeIndex > 0) {
+				// Segment hitbox changed, update tree
+				tree.remove(segment);
+				segment.end = (ItemBridgeBuild) Vars.world.build(segment.passing.items[removeIndex-1]);
+				segment.passing.setSize(removeIndex);
+				tree.insert(segment);
+			}
+			if (!segment.valid()) {
+				// REMOVEME
+				segment.end = oldEnd; // Needed as QuadTree#remove needs the Segment hitbox
+				tree.remove(segment);
+				allSegments.remove(segment);
+			}}));
+	}
+
+	public static void update() {
 		if (!Vars.state.isPlaying()) return;
 
-		// Jank here, clean up with the event based segments
-		both(QuadTree::clear);
-		allSegments.clear();
-		bridgeCache.filter(bridge -> bridge.isValid());
-		bridgeCache.forEach(ModMain2::formSegment);
-
-		allSegments.forEach(segment -> {
+		allSegments.each(segment -> {
 			segment.selfIndex = 0;
 			segment.currentSize = 4;
 			segment.occupied.clear();
 		});
-		allSegments.forEach(Segment::update);
-		allSegments.forEach(Segment::postUpdate);
+		allSegments.each(Segment::update);
+		allSegments.each(Segment::postUpdate);
+	}
+
+	static void updateEnd(Segment segment) {
+		QuadTree<Segment> tree = getTree(segment.linkDir());
+		tree.remove(segment);
+		segment.updateEnd();
+		tree.insert(segment);
 	}
 
 	public static void reloadSegments() {
 		Time.mark();
 		both(QuadTree::clear);
 		allSegments.clear();
-		bridgeCache.clear();
-//		Core.app.post(() -> {
-			Vars.indexer.allBuildings(bounds.x+bounds.width/2, bounds.y+bounds.height/2,
-					Math.max(bounds.width, bounds.height), building -> {
-						if (building instanceof ItemBridgeBuild b) bridgeCache.add(b);
-						if (building instanceof ItemBridgeBuild b && segHead(b) && linkValid(b)) {
-							Segment seg = new Segment(b);
-							allSegments.add(seg);
-							getTree(seg.linkDir()).insert(seg);
-						}
-					});
-//		});
+		Groups.build.each(building -> {
+			if (!(building instanceof ItemBridgeBuild b)) return;
+			formSegment(b);
+
+		});
 		Log.info("Segments reloaded in @ ms, @/@ segments total",
 				Time.elapsed(), allSegments.count(segment -> segment.start.team == Vars.player.team()), allSegments.size);
 	}
@@ -309,11 +293,17 @@ public class ModMain2 extends Mod {
 	public static Segment findSeg(int sx, int sy, int firstAxis) {
 		Seq<Segment> out = new Seq<>();
 		getTree((byte) (1-firstAxis)).intersect(sx, sy, 1, 1, out);
-		out.filter(segment -> segment.start.tileX() == sx && segment.start.tileY() == sy);
-		if (out.any()) return out.first();
+		Segment res = out.find(segment -> segment.start.tileX() == sx && segment.start.tileY() == sy);
+		if (res != null) return res;
+		out.clear();
 		getTree(firstAxis).intersect(sx, sy, 1, 1, out);
-		out.filter(segment -> segment.start.tileX() == sx && segment.start.tileY() == sy);
-		return out.any() ? out.first() : null;
+		return out.find(segment -> segment.start.tileX() == sx && segment.start.tileY() == sy);
+	}
+
+	public static Segment findSegStrict(int sx, int sy, int axis) {
+		Seq<Segment> out = new Seq<>();
+		getTree(axis).intersect(sx, sy, 1, 1, out);
+		return out.find(segment -> segment.start.tileX() == sx && segment.start.tileY() == sy);
 	}
 
 	public static void formSegment(int pos) {
